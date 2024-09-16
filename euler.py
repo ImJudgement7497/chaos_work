de37ed510fe365d171b982f26ebe8075f4f5b880
ImJudgement7497 <benwalshe.mason@outlook.com>
2024-09-16 18:43:23 +0100

    discovered phase plot

--- euler.py
+++ euler.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def euler(F, X, t0, tmax, dt):
+    r"""
+    Function to apply Euler's Method to solve a 1st Order Differential Equation
+    
+    F: np.array, array of functions to solve for
+    X: list, array of inital consitions for each function
+    t0: int, start time
+    tmax: int, maximum time
+    dt: float, time step
+    """
     t = np.arange(t0, tmax + dt, dt)
     x = np.zeros((len(t), len(X)))
     x[0,:] = X
